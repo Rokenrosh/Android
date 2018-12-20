8ca484d962300757650482eb081641ce3c2d9464
Kirill <rokenroshr@gmail.com>
2018-12-20 15:55:34 +0300

    Added change photo in profile edit

--- app/src/main/java/com/example/ppo_project/ProfileViewFragement.kt
+++ app/src/main/java/com/example/ppo_project/ProfileViewFragement.kt
@@ -1,17 +1,21 @@
 package com.example.ppo_project
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.widget.ImageViewCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
-import android.content.Context
-import android.util.Log
-import androidx.appcompat.widget.AppCompatTextView
 import androidx.navigation.findNavController
 import com.example.ppo_project.Models.User
+import com.example.ppo_project.Utils.PictureUtil
 import com.example.ppo_project.Utils.UserUtil
 import com.orm.SugarContext
 import com.orm.SugarRecord
@@ -22,9 +26,8 @@ class ProfileViewFragment : Fragment() {
     private var textName: AppCompatTextView? = null
     private var textSurname: AppCompatTextView? = null
     private var textPhone: AppCompatTextView? = null
-    private var textPassword: AppCompatTextView? = null
     private var textEmail: AppCompatTextView? = null
-
+    private var profileImageView: AppCompatImageView? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragement_profile, container, false)
@@ -34,15 +37,26 @@ class ProfileViewFragment : Fragment() {
         textSurname = v.findViewById(R.id.textViewSurname)
         textPhone = v.findViewById(R.id.textViewPhoneNumber)
         textEmail = v.findViewById(R.id.textViewEmail)
+        profileImageView = v.findViewById(R.id.profileImageView)
         updateUI()
         return v
     }
-
     private fun updateUI(){
         val user = UserUtil.instance.loadUser(context)
         textName?.text = user.name
         textSurname?.text = user.surname
         textPhone?.text = user.phone
         textEmail?.text = user.email
+        updatePhotoView()
+    }
+    private fun updatePhotoView(){
+        val photoFile = UserUtil.instance.getPhotoFile(context)
+        if(photoFile == null || !photoFile.exists()) {
+            profileImageView?.setImageDrawable(null)
+            return
+        }
+        //val bitmap = PictureUtil.getScaledBitmap(photoFile?.path as String, activity as AppCompatActivity)
+        val bitmap = PictureUtil.getRoundedBitMap(photoFile.path as String, activity as AppCompatActivity)
+        profileImageView?.setImageDrawable(bitmap)
     }
 }
